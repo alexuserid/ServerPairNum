c9b00e7f9b8033b3fc571d74f3ab028c2ea2b771
A <alexuserid@gmail.com>
2018-04-17 15:18:34 +0300

    Update server.go

--- server.go
+++ server.go
@@ -9,34 +9,39 @@ import (
 )
 
 var (
-	first   bool
-	second  = make(chan bool, 1)
-	randStr string
+	ch              = make(chan bool, 1)
+	first           bool
+	second          = make(chan bool, 1)
+	randStr, strRec string
+	toStr           string = "Timeout. No more connected users.\n"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if first {
-		second <- true
+	ch <- true
+	select {
+	case <-ch:
+		if first {
+			strRec = randStr + "\n"
+			second <- true
 
-	} else {
-		timeout := time.After(10 * time.Second)
-		rand.Seed(time.Now().Unix())
-		randStr = strconv.FormatInt(int64(rand.Intn(1000)), 10)
-		first = true
+		} else {
+			timeout := time.After(10 * time.Second)
+			randStr = strconv.FormatInt(int64(rand.Intn(1000)), 10)
+			first = true
 
-		select {
-		case <-second:
-		case <-timeout:
-			w.Write([]byte("Timeout. No more connected users."))
-			first = false
-			return
+			select {
+			case <-second:
+			case <-timeout:
+				strRec = toStr
+			}
 		}
+		w.Write([]byte(strRec))
+		first = false
 	}
-	w.Write([]byte(randStr))
-	first = false
 }
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
