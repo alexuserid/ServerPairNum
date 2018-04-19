0fa84ff9818f0a3d1d4ed7a0cc14776985d5bf6f
A <alexuserid@gmail.com>
2018-04-19 20:55:52 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,3 +1,5 @@
+//it's better version made by my friend
+//my version is in a previous commit
 package main
 
 import (
@@ -9,39 +11,25 @@ import (
 )
 
 var (
-	ch              = make(chan bool, 1)
-	first           bool
-	second          = make(chan bool, 1)
-	randStr, strRec string
-	toStr           string = "Timeout. No more connected users.\n"
+	cc = make(chan string)
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	ch <- true
+	timeout := time.After(10 * time.Second)
+	randStr := strconv.FormatInt(int64(rand.Intn(1000)), 10)
 	select {
-	case <-ch:
-		if first {
-			strRec = randStr + "\n"
-			second <- true
-
-		} else {
-			timeout := time.After(10 * time.Second)
-			randStr = strconv.FormatInt(int64(rand.Intn(1000)), 10)
-			first = true
-
-			select {
-			case <-second:
-			case <-timeout:
-				strRec = toStr
-			}
-		}
-		w.Write([]byte(strRec))
-		first = false
+	case ans := <-cc:
+		w.Write([]byte(ans))
+	case cc <- randStr:
+		w.Write([]byte(randStr))
+	case <-timeout:
+		w.Write([]byte("Timeout. No more connected users."))
 	}
+	w.Write([]byte("\n"))
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":2080", nil))
 }
